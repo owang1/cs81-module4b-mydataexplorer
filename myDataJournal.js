a4660732b1f281eadd06cc403aea2b762804a31c
Olivia Wang <owang1@nd.edu>
2025-07-05 19:51:26 -0500

    Define weekData array

---
+++ myDataJournal.js
@@ -0,0 +1,9 @@
+let weekData = [
+  { day: "Monday", sleepHours: 9.5, screenTime: 6, mood: "normal", caffeineIntake: 1, focusLevel: 4},
+  { day: "Tuesday", sleepHours: 8, screenTime: 4.5, mood: "high", caffeineIntake: 1, focusLevel: 4},
+  { day: "Wednesday", sleepHours: 9, screenTime: 5, mood: "normal", caffeineIntake: 2, focusLevel: 7},
+  { day: "Thursday", sleepHours: 9, screenTime: 2, mood: "normal", caffeineIntake: 3, focusLevel: 6},
+  { day: "Friday", sleepHours: 9, screenTime: 2.5, mood: "high", caffeineIntake: 1, focusLevel: 2},
+  { day: "Saturday", sleepHours: 9, screenTime: 1.5, mood: "low", caffeineIntake: 1, focusLevel: 5},
+  { day: "Sunday", sleepHours: 9, screenTime: 4, mood: "normal", caffeineIntake: 1, focusLevel: 2}
+];
